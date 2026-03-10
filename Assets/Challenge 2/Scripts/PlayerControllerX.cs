0856b9fcdbf1cba4379641c19000087f169d8425
Joana Ferreira <jjoanagguimaraes@gmail.com>
2026-03-10 11:54:19 +0000

    acabei

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool atiracao = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && atiracao == true)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            atiracao = false;
+            Invoke("tiracao", 1);
+
+        }
+    }
+    void tiracao()
+    {
+        atiracao = true;
+    }
+}
